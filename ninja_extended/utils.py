e1d70019d05495db158a7300ccabda9fa9455f87
Lucas Camphausen <lucas.camphausen@gmail.com>
2024-12-19 14:04:43 +0100

    Add utils

---
+++ ninja_extended/utils.py
@@ -0,0 +1,89 @@
+"""Module utils."""
+
+import re
+from decimal import Decimal
+
+
+def camel_to_kebap(value: str) -> str:
+    """Convert a camel case string to a kebap case string.
+
+    Args:
+        value (str): The string in camel case.
+
+    Returns:
+        str: The string in kebap case.
+    """
+    return re.sub(pattern=r"(?<!^)(?=[A-Z])", repl="-", string=value).lower()
+
+
+def snake_to_camel(value: str) -> str:
+    """Convert a snake case string to a camel case string.
+
+    Args:
+        value (str): The string in snake case.
+
+    Returns:
+        str: The string in camel case.
+    """
+    camel_str = re.sub(pattern=r"_([a-zA-Z])", repl=lambda match: match.group(1).upper(), string=value)
+
+    return camel_str[0].lower() + camel_str[1:] if camel_str else ""
+
+
+def pluralize(value: str) -> str:
+    """Pluralize a string.
+
+    Args:
+        value (str): The string in singular.
+
+    Returns:
+        str: The string in plural.
+    """
+
+    if value.endswith(("ch", "sh", "x", "s", "z", "o")):
+        return f"{value}es"
+
+    if value.endswith("y") and value[-2] not in "aeiou":
+        return f"{value[:-1]}ies"
+
+    if value.endswith("f"):
+        return f"{value[:-1]}ves"
+
+    if value.endswith("fe"):
+        return f"{value[:-2]}ves"
+
+    return f"{value}s"
+
+
+def convert_value_to_detail_string(value: bool | Decimal | float | int | str) -> str:  # noqa: PYI041
+    """Convert a value into a string representation.
+
+    Args:
+        value (bool | Decimal | float | int | str): The value to be converted.
+
+    Raises:
+        ValueError: If the value can not be converted.
+
+    Returns:
+        str: The string representation value.
+    """
+
+    if value is True:
+        return "true"
+
+    if value is False:
+        return "false"
+
+    if isinstance(value, Decimal):
+        return str(value)
+
+    if isinstance(value, float):
+        return str(value)
+
+    if isinstance(value, int):
+        return str(value)
+
+    if isinstance(value, str):
+        return f"'{value}'"
+
+    raise ValueError(f"Can't convert value of type '{type(value)}' for detail string.")  # noqa: EM102
---
+++ tests/test_utils.py
@@ -0,0 +1,63 @@
+from decimal import Decimal
+
+import pytest
+from ninja_extended.utils import camel_to_kebap, convert_value_to_detail_string, snake_to_camel, pluralize
+
+
+@pytest.mark.parametrize(
+    "camel_string,kebap_string",
+    [
+        ["foobar", "foobar"],
+        ["Foobar", "foobar"],
+        ["fooBar", "foo-bar"],
+        ["FooBar", "foo-bar"],
+        ["FooBarBaz", "foo-bar-baz"],
+    ],
+)
+def test_camel_to_kebap(camel_string: str, kebap_string: str):
+    assert camel_to_kebap(value=camel_string) == kebap_string
+
+
+@pytest.mark.parametrize(
+    "snake_string,camel_string",
+    [
+        ["foobar", "foobar"],
+        ["foo_bar", "fooBar"],
+        ["foo_bar_baz", "fooBarBaz"],
+    ],
+)
+def test_camel_to_kebap(snake_string: str, camel_string: str):
+    assert snake_to_camel(value=snake_string) == camel_string
+
+@pytest.mark.parametrize(
+    "value,output",
+    [
+        [True, "true"],
+        [False, "false"],
+        [Decimal("42.21"), "42.21"],
+        [42, "42"],
+        [42.21, "42.21"],
+        ["foo", "'foo'"],
+    ],
+)
+def test_convert_value_to_detail_string(value: bool, output: str):
+    assert convert_value_to_detail_string(value=value) == output
+
+
+@pytest.mark.parametrize(
+    "singular,plural",
+    [
+        ["couch", "couches"],
+        ["fish", "fishes"],
+        ["fox", "foxes"],
+        ["cross", "crosses"],
+        ["quartz", "quartzes"],
+        ["hero", "heroes"],
+        ["country", "countries"],
+        ["leef", "leeves"],
+        ["life", "lives"],
+        ["apple", "apples"]
+    ],
+)
+def test_pluralize(singular: str, plural: str):
+    assert pluralize(value=singular) == plural
