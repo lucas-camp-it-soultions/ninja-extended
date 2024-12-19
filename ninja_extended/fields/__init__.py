213b57ae2f1413a533111af8972f529a595eba2e
Lucas Camphausen <lucas.camphausen@gmail.com>
2024-12-19 14:39:37 +0100

    Fixfields module

---
+++ ninja_extended/fields/__init__.py
@@ -0,0 +1,10 @@
+"""Module fields."""
+
+# ruff: noqa: F401
+
+from ninja_extended.fields.bool import BoolField, BoolFieldValues
+from ninja_extended.fields.date import DateField, DateFieldValues
+from ninja_extended.fields.datetime import DatetimeField, DatetimeFieldValues
+from ninja_extended.fields.email import EmailField, EmailFieldValues
+from ninja_extended.fields.int import IntField, IntFieldValues
+from ninja_extended.fields.string import StringField, StringFieldValues
