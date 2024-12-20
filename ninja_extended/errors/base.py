a2a92fd6ddd59da2180ac3be98bf7a4f4c064181
Lucas Camphausen <lucas.camphausen@gmail.com>
2024-12-20 11:53:51 +0100

    Add registration callback for API errors

--- ninja_extended/errors/base.py
+++ ninja_extended/errors/base.py
@@ -1,6 +1,9 @@
 """Module errors.base."""
 
+from django.http import HttpRequest
 from pydantic import BaseModel
+
+from ninja_extended.api import ExtendedNinjaAPI
 
 
 class APIError(Exception):
@@ -47,3 +50,21 @@ class APIErrorResponse(BaseModel):
     detail: str | None = None
     path: str
     opertaion_id: str
+
+
+def register_exception_handler(api: ExtendedNinjaAPI, error_type: type[APIError]):
+    """Register an APIError.
+
+    Args:
+        api (ExtendedNinjaAPI): The API to regsiter the error for.
+        error_type (type[APIError]): The error type.
+    """
+
+    def _handler(request: HttpRequest, error: APIError):
+        return api.create_response(
+            request=request,
+            data=error.schema()(**error.to_dict(), path=request.path, operation_id=request.operation_id),
+            status=error.status,
+        )
+
+    api.add_exception_handler(exc_class=error_type, handler=_handler)
