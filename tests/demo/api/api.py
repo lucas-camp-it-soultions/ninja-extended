13c09744367be4472fb77c92f0cddebc86dde7f3
Lucas Camphausen <lucas.camphausen@gmail.com>
2024-12-20 10:44:33 +0100

    Extend demo

--- tests/demo/api/api.py
+++ tests/demo/api/api.py
@@ -1,9 +1,12 @@
 from django.db import IntegrityError
+from django.db.transaction import atomic
 from django.http import HttpRequest
 from ninja import Schema
 
 from ninja_extended.api import ExtendedNinjaAPI, ExtendedRouter
+from ninja_extended.errors import NotNullConstraintError, UniqueConstraintError
 from ninja_extended.errors.integrity import IntegrityErrorParser
+from ninja_extended.errors.integrity.types import IntegrityErrorType
 from ninja_extended.fields import IntField, IntFieldValues, StringField, StringFieldValues
 
 from .models import Resource
@@ -35,16 +38,56 @@ class ResourceResponse(Schema):
     value_not_null: str | None = StringField(field_values=ResourceFieldValues.value_not_null)
 
 
+class ResourceUniqueConstraintError(UniqueConstraintError):
+    resource_name = "Resource"
+
+
+class ResourceNotNullConstraintError(NotNullConstraintError):
+    resource_name = "Resource"
+
+
+@api.exception_handler(ResourceUniqueConstraintError)
+def resource_unique_constraint_error_handler(request: HttpRequest, error: ResourceUniqueConstraintError):
+    return api.create_response(
+        request=request,
+        data=ResourceUniqueConstraintError.schema()(**error.to_dict(), path=request.path, operation_id=request.operation_id),
+        status=ResourceUniqueConstraintError.status,
+    )
+
+
+@api.exception_handler(ResourceNotNullConstraintError)
+def resource_not_null_constraint_error_handler(request: HttpRequest, error: ResourceNotNullConstraintError):
+    return api.create_response(
+        request=request,
+        data=ResourceNotNullConstraintError.schema()(**error.to_dict(), path=request.path, operation_id=request.operation_id),
+        status=ResourceNotNullConstraintError.status,
+    )
+
+
 @router.post(
-    path="/",
+    path="/resources",
     operation_id="createResource",
     summary="Create a resource",
     description="Create a resource",
     tags=["resources"],
-    response=ResourceResponse,
+    response={
+        201: ResourceResponse,
+        422: ResourceNotNullConstraintError.schema() | ResourceUniqueConstraintError.schema(),
+    },
 )
 def create_resource(request: HttpRequest, data: ResourceCreateRequest):  # noqa: ARG001
-    return Resource.objects.create(**data.model_dump())
+    with atomic():
+        try:
+            return 201, Resource.objects.create(**data.model_dump())
+        except IntegrityError as error:
+            print("INSIDE ERROR HANDLER")
+            type, columns = IntegrityErrorParser().parse(error=error)
+
+            if type == IntegrityErrorType.UNIQUE_CONSTRAINT:
+                raise ResourceUniqueConstraintError({key: data.model_dump()[key] for key in columns})
+
+            if type == IntegrityErrorType.NOT_NULL_CONSTRAINT:
+                raise ResourceNotNullConstraintError({key: data.model_dump()[key] for key in columns})
 
 
 api.add_router("", router)
