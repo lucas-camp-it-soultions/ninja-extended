68686f1ecd35e60f1b5a441fb6e597b78dda605f
Lucas Camphausen <lucas.camphausen@gmail.com>
2024-12-20 11:54:33 +0100

    Add validation error

--- ninja_extended/errors/__init__.py
+++ ninja_extended/errors/__init__.py
@@ -2,7 +2,8 @@
 
 # ruff: noqa: F401
 
-from ninja_extended.errors.base import APIError, APIErrorResponse
+from ninja_extended.errors.base import APIError, APIErrorResponse, register_exception_handler
 from ninja_extended.errors.not_found import NotFoundError
 from ninja_extended.errors.not_null_constraint import NotNullConstraintError
 from ninja_extended.errors.unique_constraint import UniqueConstraintError
+from ninja_extended.errors.validation import ValidationError, ValidationErrorDetail
---
+++ ninja_extended/errors/validation.py
@@ -0,0 +1,132 @@
+"""Module error.not_found."""
+
+from typing import Annotated, Any, Literal
+
+from pydantic import BaseModel, Field, create_model
+
+from ninja_extended.errors.base import APIError
+from ninja_extended.utils import camel_to_kebap, camel_to_pascal
+
+
+class ValidationErrorDetail(BaseModel):
+    """Schema for Ninja ValidationError."""
+
+    type: str
+    loc: tuple[str | int, ...]
+    msg: str
+    ctx: dict[str, Any] | None
+
+
+class ValidationError(APIError):
+    """Base not found error class."""
+
+    operation_id: str
+    status: int = 422
+
+    def __init__(self, errors: list[ValidationErrorDetail]):
+        """Initialize a ValidationError."""
+
+        error_type = f"errors/{camel_to_kebap(value=self.operation_id)}/validation"
+        title = f"Validation for operation {self.operation_id} failed."
+        detail = f"Validation for operation {self.operation_id} failed."
+
+        super().__init__(type=error_type, title=title, detail=detail)
+
+        self.errors = errors
+
+    def to_dict(self):
+        """Serialize the NotFoundError."""
+
+        base_dict = super().to_dict()
+        base_dict.update({"errors": self.errors})
+
+        return base_dict
+
+    @classmethod
+    def schema(cls):
+        """Return the schema for the error.
+
+        Returns:
+            type[BaseModel]: The schema.
+        """
+
+        model_name = f"{camel_to_pascal(value=cls.operation_id)}ValidationErrorResponse"
+        error_type = f"errors/{camel_to_kebap(value=cls.operation_id)}/validation"
+        title = f"Validation for operation {cls.operation_id} failed."
+        detail = f"Validation for operation {cls.operation_id} failed."
+
+        return create_model(
+            model_name,
+            type=(
+                Literal[error_type],
+                Field(description=f"The type of the {model_name}."),
+            ),
+            status=(
+                Literal[cls.status],
+                Field(description=f"The status of the {model_name}."),
+            ),
+            title=(
+                Literal[title],
+                Field(description=f"The title of the {model_name}."),
+            ),
+            detail=(
+                Literal[detail],
+                Field(description=f"The detail of the {model_name}."),
+            ),
+            errors=(
+                list[ValidationErrorDetail],
+                Field(description=f"The error details of the {model_name}."),
+            ),
+            path=(
+                str,
+                Field(description=f"The path of the {model_name}."),
+            ),
+            operation_id=(
+                str,
+                Field(description=f"The operation id of the {model_name}."),
+            ),
+        )
+
+
+def validation_error_factory(operation_id: str) -> type[APIError]:
+    """Create a ValidationError dynamically.
+
+    Args:
+        operation_id (str): The operation id.
+
+    Returns:
+        type[APIError]: The dynamic ValidationError type.
+    """
+
+    return type(
+        f"{camel_to_pascal(value=operation_id)}ValidationError", (ValidationError,), {"operation_id": operation_id}
+    )
+
+
+def discriminate_validation_errors(error_types: list[type[APIError]]):
+    """Build a discriminated union of error type.
+
+    Args:
+        error_types (list[type[APIError]]): The error types.
+
+    Raises:
+        RuntimeError: If no error types are provided.
+
+    Returns:
+        _type_: The disciminated union of error types.
+    """
+
+    error_message_no_types = "At least one error type must be provided"
+
+    if len(error_types) == 0:
+        raise RuntimeError(error_message_no_types)
+
+    schema = error_types[0].schema()
+
+    if len(error_types) == 1:
+        return schema
+
+    for error_type in error_types[1:]:
+        schema |= error_type.schema()
+
+    return Annotated[schema, Field(discriminator="type")]
