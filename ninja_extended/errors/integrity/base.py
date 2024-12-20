3e1325482df52ea1ac34edadcf4537247e00fa08
Lucas Camphausen <lucas.camphausen@gmail.com>
2024-12-20 12:54:42 +0100

    Add generic constraint error handler

--- ninja_extended/errors/integrity/base.py
+++ ninja_extended/errors/integrity/base.py
@@ -1,6 +1,7 @@
 """Module errors.integrity.base."""
 
 from sqlite3 import IntegrityError as SQLite3IntegrityError
+from typing import Any
 
 from django.db import IntegrityError
 from psycopg2.errors import NotNullViolation, UniqueViolation
@@ -8,6 +9,8 @@ from psycopg2.errors import NotNullViolation, UniqueViolation
 from ninja_extended.errors.integrity.postgres import PostgresIntegrityErrorParser
 from ninja_extended.errors.integrity.sqlite3 import SQLite3IntegrityErrorParser
 from ninja_extended.errors.integrity.types import IntegrityErrorType
+from ninja_extended.errors.not_null_constraint import NotNullConstraintError
+from ninja_extended.errors.unique_constraint import UniqueConstraintError
 
 
 class IntegrityErrorParser:
@@ -35,3 +38,31 @@ class IntegrityErrorParser:
             return SQLite3IntegrityErrorParser().parse(error=error)
 
         raise RuntimeError(parse_error_message_unknown_vendor)
+
+
+def handle_integrity_error(
+    error: IntegrityError,
+    unique_constraint_error_type: type[UniqueConstraintError],
+    not_null_constraint_error_type: type[NotNullConstraintError],
+    data: Any = None,
+):
+    """Handle IntegrityError.
+
+    Args:
+        error (IntegrityError): The error.
+        unique_constraint_error_type (type[UniqueConstraintError]): The unique constraint error type.
+        not_null_constraint_error_type (type[NotNullConstraintError]): The not null constraint error type.
+        data (Any): The data.
+
+    Raises:
+        unique_constraint_error_type: If a unique constraint error have been parsed.
+        not_null_constraint_error_type: If a not null constraint error have been parsed.
+    """
+
+    integrity_error_type, columns = IntegrityErrorParser().parse(error=error)
+
+    if integrity_error_type == IntegrityErrorType.UNIQUE_CONSTRAINT:
+        raise unique_constraint_error_type({key: data.model_dump()[key] for key in columns})
+
+    if integrity_error_type == IntegrityErrorType.NOT_NULL_CONSTRAINT:
+        raise not_null_constraint_error_type({key: data.model_dump()[key] for key in columns})
