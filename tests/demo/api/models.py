c3d18c5e00062f7225b981c33e0418120345a2e8
Lucas Camphausen <lucas.camphausen@iml.fraunhofer.de>
2025-01-22 15:05:18 +0100

    Explicitly set null and blank attributes for fields

--- tests/demo/api/models.py
+++ tests/demo/api/models.py
@@ -156,9 +156,9 @@ class Resource(Model):
         ]
 
     id = AutoField(primary_key=True, unique=True, editable=False)
-    value_unique = CharField(max_length=32, unique=True)
-    value_unique_together_1 = CharField(max_length=32)
-    value_unique_together_2 = CharField(max_length=32)
+    value_unique = CharField(max_length=32, unique=True, null=False, blank=False)
+    value_unique_together_1 = CharField(max_length=32, null=False, blank=False)
+    value_unique_together_2 = CharField(max_length=32, null=False, blank=False)
     value_not_null = CharField(max_length=32, null=False, blank=False)
     value_check = IntegerField(null=True)
 
